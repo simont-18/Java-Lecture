fec3e03e4ea96120bbd91447dcdadacf4d16f7a2
Simon T <duy.ta1895@gmail.com>
2014-03-18 11:13:57 -0500

    Extra program #5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-	public static void main(String args[]) {
-		
+	 public static String lessThan100( int number ) throws Exception {
+		if (number >=100)
+			throw new Exception("over 100!");// write your code here;
+		return String.format("The Number %d is less than 100",number);
+	}
+	
+	public static void main( String args[] ) {
+		try {
+			System.out.println(lessThan100(89));
+			System.out.println(lessThan100(819));
+
+		}
+		catch( Exception exception ) {
+			System.out.println( exception.toString() ); 
+		}
 	}
 }
