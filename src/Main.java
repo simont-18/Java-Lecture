679789f822f4133851423fed3297fe9fc41a4989
Simon T <duy.ta1895@gmail.com>
2014-03-18 23:04:30 -0500

    Extra program #3

--- src/Main.java
+++ src/Main.java
@@ -1,102 +1,90 @@
-import java.util.Random;      //................
+import java.awt.Point;
+
+import javax.swing.JOptionPane;
+
+
 public class Main {
-	public static void main(String[] args) {
-		Random generator = new Random();             //................
-		point A=new point();   // call of default const
-		point B=new point(7,11); // using const with arg
+	public static void main(String[] args)
+	{
+  		int x = 0, y = 0, sl = 0;
+  		
+		x = getNumber("Enter x coordinate: ");	
+		y = getNumber("Enter y coordinate: ");
+		sl = getNumber("Enter side of Square: ");
 		
-		point p=new point();
+		//Instantiate the classes
+		Point myPoint = new Point(x, y);
+  		Square mySquare = new Square(x, y, sl);
+  		Cube myCube = new Cube(x, y, sl);
+
+  		//Display the output
+  		JOptionPane.showMessageDialog(null, "Point:\n" + myPoint.toString()+
+  				"\nSquare:\n" + myPoint.toString() + mySquare.toString()+
+  				"\nCube:\n" + myPoint.toString()+ myCube.toString(),
+  				"Result", JOptionPane.INFORMATION_MESSAGE); 		
+	}	
+	
+	public static int getNumber(String str)
+	{
+		String numStr = "";
+		int num = 0;
+		boolean isValid = false;
 		
-		point q=new point(4,5);
-		
-		point k=new point(q);
-		
-		A.setxy(50,25);
-		A.print();
-		
-		System.out.println(" the new value of x is "+A.getX() +"\n and of Y is "+
-				A.getY());
-		
-		// an array of points
-		
-		point table[] = new point[10];
-		// fill it randomly .......
-		for(int i=0;i<10;i++)
-		{   
-			table[i]=new point();   ///////////
-			table[i].setxy(generator.nextInt() % 100,generator.nextInt() %250  );
-			table[i].print();
-		}	
+		//while input is not valid
+		while(!isValid)
+		{
+			numStr = JOptionPane.showInputDialog(str);
+			
+			if (numStr == null)
+			{
+				JOptionPane.showMessageDialog(null, "You must enter a valid value. Please try again", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+			else
+			{
+				try
+			  	{
+		  			//If the number entered is not a number, an error will occur
+		  			num = Integer.parseInt(numStr);
+		  			isValid = true;
+		  			
+			  	}
+			  	catch (NumberFormatException nfe)
+			  	{
+		  			//Execute this code if an error is detected
+		  			JOptionPane.showMessageDialog(null, "The value you entered is not a valid number. Please try again", "Error", JOptionPane.ERROR_MESSAGE);
+			  	}
+			}
+		}
+	  	
+	  	return num;
 	}
 }
 
-class point {
-	private int x, y;
-	point() {
-		x = 0;
-		y = 0;
+class Square extends Point {
+	protected int sl;
+	Square(int x, int y, int sl) {
+		super(x,y);
+		this.sl = sl;
 	}
 	
-	point(int x, int y) {
-		setxy(x,y);
-	}
-	
-	point(point a) {
-		setX(a.getX());
-		setY(a.getY());
-	}
-	
-	public void setxy(int x, int y) {
-		setX(x);
-		setY(y);
+	public int getSl() {
+		return sl;
 	}
 	
 	
-	public int getX() {
-		return x;
+	@Override
+	public String toString() {
+		return "Square [x=" + x + ", y=" + y + ", sl=" + sl + "]";
 	}
+}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public void print() {
-		System.out.println("point [x=" + x + ", y=" + y + "]");
+class Cube extends Square {
+	Cube(int x, int y, int sl) {
+		super(x,y,sl);
 	}
 	
 	@Override
 	public String toString() {
-		return "point [x=" + x + ", y=" + y + "]";
+		return "Cube [x=" + x + ", y=" + y + ", sl=" + sl + "]";
 	}
 }
-
-//--------------------------------------------------------------------------------------------------
-
-//extra #5
-
-//public class Main {
-//	 public static String lessThan100( int number ) throws Exception {
-//		if (number >=100)
-//			throw new Exception("over 100!");// write your code here;
-//		return String.format("The Number %d is less than 100",number);
-//	}
-//	
-//	public static void main( String args[] ) {
-//		try {
-//			System.out.println(lessThan100(89));
-//			System.out.println(lessThan100(819));
-//
-//		}
-//		catch( Exception exception ) {
-//			System.out.println( exception.toString() ); 
-//		}
-//	}
-//}
