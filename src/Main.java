ed9de7e10338a520f671f3feaf64c8a3e69ddba2
Simon T <duy.ta1895@gmail.com>
2014-03-29 00:34:24 -0500

    writing even and odd numbers to file and read them back in

--- src/Main.java
+++ src/Main.java
@@ -1,90 +1,48 @@
-import java.awt.Point;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
-
 public class Main {
-	public static void main(String[] args)
-	{
-  		int x = 0, y = 0, sl = 0;
-  		
-		x = getNumber("Enter x coordinate: ");	
-		y = getNumber("Enter y coordinate: ");
-		sl = getNumber("Enter side of Square: ");
-		
-		//Instantiate the classes
-		Point myPoint = new Point(x, y);
-  		Square mySquare = new Square(x, y, sl);
-  		Cube myCube = new Cube(x, y, sl);
 
-  		//Display the output
-  		JOptionPane.showMessageDialog(null, "Point:\n" + myPoint.toString()+
-  				"\nSquare:\n" + myPoint.toString() + mySquare.toString()+
-  				"\nCube:\n" + myPoint.toString()+ myCube.toString(),
-  				"Result", JOptionPane.INFORMATION_MESSAGE); 		
-	}	
-	
-	public static int getNumber(String str)
-	{
-		String numStr = "";
-		int num = 0;
-		boolean isValid = false;
+	public static void main(String[] args) throws IOException {
+		//write even numbers to file
+		writeNumbers(0, false);
+		JOptionPane.showMessageDialog(null, "Even numbers written to the file. Press OK to display.", "Message", JOptionPane.INFORMATION_MESSAGE);
+		displayContents();
 		
-		//while input is not valid
-		while(!isValid)
-		{
-			numStr = JOptionPane.showInputDialog(str);
-			
-			if (numStr == null)
-			{
-				JOptionPane.showMessageDialog(null, "You must enter a valid value. Please try again", "Error", JOptionPane.ERROR_MESSAGE);
-			}
-			else
-			{
-				try
-			  	{
-		  			//If the number entered is not a number, an error will occur
-		  			num = Integer.parseInt(numStr);
-		  			isValid = true;
-		  			
-			  	}
-			  	catch (NumberFormatException nfe)
-			  	{
-		  			//Execute this code if an error is detected
-		  			JOptionPane.showMessageDialog(null, "The value you entered is not a valid number. Please try again", "Error", JOptionPane.ERROR_MESSAGE);
-			  	}
-			}
+		//append odd numbers to file
+		writeNumbers(1, true);
+		JOptionPane.showMessageDialog(null, "Odd numbers appended to the file. Press OK to display.", "Message", JOptionPane.INFORMATION_MESSAGE);
+		displayContents();
+
+	}
+	
+	public static void writeNumbers(int start, boolean append) throws IOException{
+		PrintWriter out = new PrintWriter(new FileOutputStream("numbers.dat", append));
+		
+		if(append == true)
+			out.println();
+		
+		for(int i = start; i <= 100; i+=2) {
+			out.print(i + " ");
 		}
-	  	
-	  	return num;
-	}
-}
-
-class Square extends Point {
-	protected int sl;
-	Square(int x, int y, int sl) {
-		super(x,y);
-		this.sl = sl;
+		
+		if(out != null)
+			out.close();
 	}
 	
-	public int getSl() {
-		return sl;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "Square [x=" + x + ", y=" + y + ", sl=" + sl + "]";
-	}
-}
-
-class Cube extends Square {
-	Cube(int x, int y, int sl) {
-		super(x,y,sl);
-	}
-	
-	@Override
-	public String toString() {
-		return "Cube [x=" + x + ", y=" + y + ", sl=" + sl + "]";
+	public static void displayContents() throws IOException {
+		Scanner in = new Scanner(new FileInputStream("numbers.dat"));
+		
+		while(in.hasNext()) {
+			System.out.println(in.nextLine());
+		}
+		
+		if(in != null)
+			in.close();
 	}
 }
